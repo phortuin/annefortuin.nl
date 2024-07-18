7d0e182d5029933a4347460e795b7bdfdf26436a
phortuin <phortuin@users.noreply.github.com>
2024-07-18 16:38:34 +0200

    Add date functions

--- lib/date.ts
+++ lib/date.ts
@@ -1,10 +1,43 @@
-export default function date(date:Date) {
-	const d = new Date(date);
-	let options: Intl.DateTimeFormatOptions = {
-	  weekday: "long",
-	  year: "numeric",
-	  month: "long",
-	  day: "numeric",
-	};
-	return new Intl.DateTimeFormat('en-GB', options).format(d)
+const LOCALE = "en";
+
+/**
+ * Get ISO date without time (yyyy-mm-dd) for Date object
+ * @returns {string} yyyy-mm-dd
+ */
+export const dateToYMD = (date:Date) => date.toISOString().slice(0, 10);
+
+export function formatDate(date:Date) {
+	const formatter = new Intl.DateTimeFormat(LOCALE, {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	});
+	return formatter.format(date);
+}
+
+/**
+ * Return a human-readable relative time string
+ * @see https://stackoverflow.com/a/72817357
+ */
+export function timeAgo(date:Date) {
+	const formatter = new Intl.RelativeTimeFormat(LOCALE);
+	const ranges = [
+		["years", 3600 * 24 * 365],
+		["months", 3600 * 24 * 30],
+		["weeks", 3600 * 24 * 7],
+		["days", 3600 * 24],
+		["hours", 3600],
+		["minutes", 60],
+		["seconds", 1],
+	] as const;
+	const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+
+	for (const [type, duration] of ranges) {
+		if (duration < Math.abs(secondsElapsed)) {
+			const delta = secondsElapsed / duration;
+			return formatter.format(Math.round(delta), type);
+		}
+ 	}
+	return "A long time ago";
 }
