2439da2af016839e857c0686cac25dd0ddd8994f
phortuin <phortuin@users.noreply.github.com>
2025-06-25 10:24:39 +0200

    Update dependencies
    - Remove dotenv and replace with Vite’s loadEnv function

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,13 @@
-// Astro
 import { defineConfig } from "astro/config";
 import { type RemarkPlugins } from "astro";
 import mdx from "@astrojs/mdx";
 
-// NPM
-import { config } from "dotenv-safe";
 import remarkWikiLink from "remark-wiki-link";
 import slugify from "slugify";
+import { loadEnv } from "vite";
 
-// Initialise dotenv
-config();
+const { SITE_URL } = loadEnv(process.env.SITE_URL, process.cwd(), "");
 
-// Astro config
 export default defineConfig({
 	devToolbar: {
 		enabled: false,
@@ -22,14 +18,11 @@ export default defineConfig({
 		},
 		remarkPlugins: remarkPlugins(),
 	},
-	site: process.env.SITE_URL,
+	site: SITE_URL,
 	trailingSlash: "always",
 	integrations: [mdx()],
 });
 
-/**
- * Wrapper function to keep Astro config somewhat clean
- */
 function remarkPlugins(): RemarkPlugins {
 	const options = {
 		aliasDivider: "|",
